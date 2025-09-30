efd5740755e9d95241df3029678c54ced5db709e
nzhukovskiy <zhukovskiy.sfedu@gmail.com>
2025-10-01 01:04:40 +0300

    [FRONT] Fixed no changes case

--- encyclopedia-web/src/app/features/history/components/article-properties-history/article-properties-history.component.ts
+++ encyclopedia-web/src/app/features/history/components/article-properties-history/article-properties-history.component.ts
@@ -1,20 +1,35 @@
 import {AfterViewInit, Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {Article} from '../../../articles/models/article';
-import { HistoryDiffComponent } from '../history-diff/history-diff.component';
+import {HistoryDiffComponent} from '../history-diff/history-diff.component';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-article-properties-history',
-  imports: [HistoryDiffComponent],
-  templateUrl: './article-properties-history.component.html',
-  styleUrl: './article-properties-history.component.scss'
+    selector: 'app-article-properties-history',
+    imports: [HistoryDiffComponent],
+    templateUrl: './article-properties-history.component.html',
+    styleUrl: './article-properties-history.component.scss'
 })
-export class ArticlePropertiesHistoryComponent implements AfterViewInit {
+export class ArticlePropertiesHistoryComponent implements AfterViewInit, OnInit {
+
+    constructor(private readonly route: ActivatedRoute) {
+    }
 
     differencesLoaded = false;
-    hasDifferences: boolean = false;
+    hasDifferences = false;
+
+    ngOnInit(): void {
+        this.route.params.subscribe(() => {
+            this.differencesLoaded = false;
+            this.hasDifferences = false;
+
+            setTimeout(() => {
+                this.checkDifferences();
+            });
+        });
+    }
 
     ngAfterViewInit(): void {
-      this.checkDifferences();
+        this.checkDifferences();
     }
 
     @Input() previousArticle?: Article;
@@ -23,12 +38,7 @@ export class ArticlePropertiesHistoryComponent implements AfterViewInit {
     @ViewChildren('historyDiff') diffDisplayers!: QueryList<HistoryDiffComponent>;
 
     checkDifferences() {
-      // console.log(this.diffDisplayers)
-      // if (this.diffDisplayers) {
-      //   return this.diffDisplayers?.some(displayer => displayer.shouldShowDiff) ?? false;
-      // }
-      // return false;
-      this.differencesLoaded = true;
-      this.hasDifferences = this.diffDisplayers?.some(displayer => displayer.shouldShowDiff) ?? false;
+        this.differencesLoaded = true;
+        this.hasDifferences = this.diffDisplayers?.some(displayer => displayer.shouldShowDiff) ?? false;
     }
 }
